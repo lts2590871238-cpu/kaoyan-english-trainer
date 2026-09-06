83f5992ad689a8fe63a3cb7132eb8a3b9d155adc
lts2590871238-cpu <lts2590871238@gmail.com>
2026-09-06 13:27:00 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.XUANXUAN_CONFIG = {
- AI_PROXY_URL: "https://xuanxuan-english-ai.lts2590871238.workers.dev",
-  DEFAULT_ACCENT: "en-US"
+  AI_PROXY_URL: "https://xuanxuan-english-ai.lts2590871238.workers.dev",
+  DEFAULT_ACCENT: "en-US",
+  APP_VERSION: "v20.0.0"
 };
